05b005c8591e44014f996ad76245b124cec3d0ee
zandrah <alizandrahdeguzman08@gmail.com>
2025-04-07 18:05:22 +0800

    add file

---
+++ Students.java
@@ -0,0 +1,22 @@
+public class Students extends Person {
+    String gradeLevel;
+ 
+    public Students(String name, int id, String gradeLevel) {
+        super(name, id);
+        this.gradeLevel = gradeLevel;  
+    }
+ 
+    public String getGradeLevel() {
+        return gradeLevel;
+    }
+ 
+    public void setGradeLevel(String gradeLevel) {
+        this.gradeLevel = gradeLevel;
+    }
+@Override
+ public void displayinfo() {
+    System.out.println("Grade Level: " + gradeLevel);
+ }
+
+}
+ 
